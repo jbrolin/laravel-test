0cf878241e7ba3ca65a45ac65356be19c0d5db47
jbrolin <jonas@nimblr.se>
2022-09-08 15:49:55 +0200

    Update to use database

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     @foreach ($posts as $post)
         <article>
-            <a href="/posts/{{ $post->slug }}">
+            <a href="/posts/{{ $post->id }}">
                 <h1>{{ $post->title }}</h1>
             </a>
             <div>
